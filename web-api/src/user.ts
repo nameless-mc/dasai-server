56e7fa00301b716797baca38cb4e94261c7c857e
Nameless <tanisyun0331@icloud.com>
2022-04-23 11:42:10 +0900

    setup_api_server: サンプルの実装

---
+++ web-api/src/user.ts
@@ -0,0 +1,13 @@
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+const users: User[] = [
+  { id: 1, name: "User1", email: "user1@test.local" },
+  { id: 2, name: "User2", email: "user2@test.local" },
+  { id: 3, name: "User3", email: "user3@test.local" },
+];
+
+export default users;
